d0d242ca1bc4ba788a05ddd2d38592fa828c82a8
judymoody59 <jmj.chai@gmail.com>
2025-02-03 08:13:52 +0900

    Feat: 예외처리 추가

--- movieboard/src/main/java/movieboard/MovieboardApplication.java
+++ movieboard/src/main/java/movieboard/MovieboardApplication.java
@@ -1,9 +1,13 @@
 package movieboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
 public class MovieboardApplication {
 
 	public static void main(String[] args) {
